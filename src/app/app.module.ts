e1041b275d8bfeb87e4d0d6d51dc3f7ed38399f0
Unknown <martez_conner@outlook.com>
2018-01-26 02:06:30 -0800

    Finished setting up components and saving to local storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 
 import { AppComponent } from './app.component';
-import { UserComponent } from './components/user/user.component'
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LogFormComponent } from './components/log-form/log-form.component';
+import { LogsComponent } from './components/logs/logs.component';
+import { LogService } from './services/log.service'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    NavbarComponent,
+    LogFormComponent,
+    LogsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [LogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
